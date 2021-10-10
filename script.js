7ab25a12f758189232651253505607dc50d2e65c
0rGaan1c <nathan4drake1999@gmail.com>
2021-10-10 12:33:24 +0530

    fixed the hamburger menu not visible in smaller screens

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
-function sendMail(params){
-    var tempParams = {
-        from_name: document.getElementById("fromName").value,
-        email_sender: document.getElementById("emailSender").value,
-        subject_sender: document.getElementById("subjectSender").value,
-        message_sender: document.getElementById("message").value,
-    };
-    emailjs.send('{{SERVICE ID}}','{{TEMPLATE ID}}',tempParams)
-     .then(function(res){
-        Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'Mail Sent Successfully!',
-            showConfirmButton: false,
-            timer: 1500,
-          });
-     });
+function sendMail(params) {
+  var tempParams = {
+    from_name: document.getElementById("fromName").value,
+    email_sender: document.getElementById("emailSender").value,
+    subject_sender: document.getElementById("subjectSender").value,
+    message_sender: document.getElementById("message").value,
+  };
+  emailjs
+    .send("{{SERVICE ID}}", "{{TEMPLATE ID}}", tempParams)
+    .then(function (res) {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Mail Sent Successfully!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    });
 }
 
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $(".navbar").addClass("sticky");
-        } else {
-            $(".navbar").removeClass("sticky");
-        }
-    });
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+    $(".menu-btn span").toggleClass("active");
+  });
 
-    // typing animation script
-    var typed = new Typed(".typing",{
-        strings: ["Your Skill","Your Hobby","Your Passion","Your Proficiency"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true,
-        fadeOut: true,
-        
-    });
-    var typed = new Typed(".typing-2",{
-        strings: ["Your Skill","Your Hobby","Your Passion","Your Proficiency"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true,
-    });
+  // typing animation script
+  var typed = new Typed(".typing", {
+    strings: ["Your Skill", "Your Hobby", "Your Passion", "Your Proficiency"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+    fadeOut: true,
+  });
+  var typed = new Typed(".typing-2", {
+    strings: ["Your Skill", "Your Hobby", "Your Passion", "Your Proficiency"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 });
-
